09a8fa570501975dbfc4d34ed4d63e29239fb8b3
DavidChoi76 <yc5ef@virginia.edu>
2021-01-04 23:03:50 -0500

    add parameter trial modules

--- pysumma/__init__.py
+++ pysumma/__init__.py
@@ -9,6 +9,6 @@ from .force_file_list import ForcingList
 from . import utils
 from .calibration import Ostrich, OstrichParam
 from . import hydroshare_utils
-from .preprocessing import LocalAttributes, InitialCondition
+from .preprocessing import LocalAttributes, InitialCondition, ParamTrial
 from .preprocessing import gis_code
 from .preprocessing import input_netcdf
